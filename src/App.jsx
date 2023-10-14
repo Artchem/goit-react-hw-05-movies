a476e06e01651e4dfcc67b2fbeaaf3c3869b68e1
Artchem <chemerys.artem@gmail.com>
2023-10-14 14:59:18 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import Cast from 'components/Cast/Cast';
-import Layout from 'components/Layout/Layout';
-import MovieDetails from 'pages/MovieDetails/MovieDetails';
-import Reviews from 'components/Reviews/Reviews';
-import Home from 'pages/Home/Home';
-import Movies from 'pages/Movies/Movies';
-import { lazy } from 'react';
-import { Route, Routes } from 'react-router-dom';
+// import Cast from 'components/Cast/Cast';
+// import Layout from 'components/Layout/Layout';
+// import MovieDetails from 'pages/MovieDetails/MovieDetails';
+// import Reviews from 'components/Reviews/Reviews';
+// import Home from 'pages/Home/Home';
+// import Movies from 'pages/Movies/Movies';
+// import { lazy } from 'react';
+// import { Route, Routes } from 'react-router-dom';
 
 // const Home = lazy(() => import(''));
 // const Movies = lazy(() => import('..'));
@@ -13,7 +13,7 @@ import { Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <div>
-      <Routes>
+      {/* <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="movies" element={<Movies />} />
@@ -22,7 +22,7 @@ function App() {
             <Route path="reviews" element={<Reviews />} />
           </Route>
         </Route>
-      </Routes>
+      </Routes> */}
     </div>
   );
 }
