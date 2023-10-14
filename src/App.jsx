92f9e51498a5bd5e691d256e89dcf67b7ae960f3
Artchem <chemerys.artem@gmail.com>
2023-10-14 15:29:08 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Route, Routes } from 'react-router-dom';
 import Cast from 'components/Cast/Cast';
 import Layout from 'components/Layout/Layout';
 import MovieDetails from 'pages/MovieDetails/MovieDetails';
@@ -5,24 +6,23 @@ import Reviews from 'components/Reviews/Reviews';
 import Home from 'pages/Home/Home';
 import Movies from 'pages/Movies/Movies';
 // import { lazy } from 'react';
-import { Route, Routes } from 'react-router-dom';
 
 // const Home = lazy(() => import(''));
 // const Movies = lazy(() => import('..'));
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="movies" element={<Movies />} />
-        <Route path="movies/:movieId" element={<MovieDetails />}>
-          <Route path="cast" element={<Cast />} />
-          <Route path="reviews" element={<Reviews />} />
-        </Route>
-        <Route path="*" element={<Home />} />
-      </Route>
-    </Routes>
+    // <Routes>
+    //   <Route path="/" element={<Layout />}>
+    //     <Route index element={<Home />} />
+    //     <Route path="movies" element={<Movies />} />
+    //     <Route path="movies/:movieId" element={<MovieDetails />}>
+    //       <Route path="cast" element={<Cast />} />
+    //       <Route path="reviews" element={<Reviews />} />
+    //     </Route>
+    //     <Route path="*" element={<Home />} />
+    //   </Route>
+    // </Routes>
   );
 };
 
