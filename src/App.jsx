6513be1cbcc67586898385ef91df8c56f66f5629
Artchem <chemerys.artem@gmail.com>
2023-10-14 15:33:40 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// import { Route, Routes } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
 // import Cast from 'components/Cast/Cast';
 // import Layout from 'components/Layout/Layout';
 // import MovieDetails from 'pages/MovieDetails/MovieDetails';
@@ -12,18 +12,17 @@
 
 const App = () => {
   return (
-    <></>
-    // <Routes>
-    //   <Route path="/" element={<Layout />}>
-    //     <Route index element={<Home />} />
-    //     <Route path="movies" element={<Movies />} />
-    //     <Route path="movies/:movieId" element={<MovieDetails />}>
-    //       <Route path="cast" element={<Cast />} />
-    //       <Route path="reviews" element={<Reviews />} />
-    //     </Route>
-    //     <Route path="*" element={<Home />} />
-    //   </Route>
-    // </Routes>
+    <Routes>
+      {/* <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="movies" element={<Movies />} />
+        <Route path="movies/:movieId" element={<MovieDetails />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+        <Route path="*" element={<Home />} />
+      </Route> */}
+    </Routes>
   );
 };
 
