e03b2c957e571e2e8900785abd302078b762cf8d
Artchem <chemerys.artem@gmail.com>
2023-10-14 15:13:46 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,19 +12,17 @@ import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="movies" element={<Movies />} />
-          <Route path="movies/:movieId" element={<MovieDetails />}>
-            <Route path="cast" element={<Cast />} />
-            <Route path="reviews" element={<Reviews />} />
-          </Route>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="movies" element={<Movies />} />
+        <Route path="movies/:movieId" element={<MovieDetails />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
         </Route>
-        <Route path="*" element={<Home />} />
-      </Routes>
-    </>
+      </Route>
+      <Route path="*" element={<Home />} />
+    </Routes>
   );
 }
 
