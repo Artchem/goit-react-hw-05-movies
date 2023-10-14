d2fd3a4bec01ff8b429fa504cdeb085255ca90c0
Artchem <chemerys.artem@gmail.com>
2023-10-14 17:17:18 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,14 @@ import { Route, Routes } from 'react-router-dom';
 import { lazy } from 'react';
 
 const Home = lazy(() => import('./pages/Home/Home'));
-// const Movies = lazy(() => import('..'));
+const Movies = lazy(() => import('./pages/Movies/Movies'));
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<div />}>
         <Route index element={<Home />} />
-        <Route path="movies" element={<div />} />
+        <Route path="movies" element={<Movies />} />
         <Route path="movies/:movieId" element={<div />}>
           <Route path="cast" element={<div />} />
           <Route path="reviews" element={<div />} />
